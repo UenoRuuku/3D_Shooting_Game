a75b9f36aab44e744a25762f539ecf3a2ed566ab
zachzhou31 <105928767+zachzhou31@users.noreply.github.com>
2023-04-10 16:08:46 -0400

    4/10

--- Assets/Basic Script/PlayerController.cs
+++ Assets/Basic Script/PlayerController.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float _rotateSpeed;
     private Rigidbody rb;
     public Camera cam;
 
-    Vector3 movement;
-    Vector3 mousePos;
+    Vector2 movement;
+    Vector2 mousePos;
     private void Start()
     {
         rb = this.GetComponent<Rigidbody>();
@@ -26,7 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         rb.MovePosition(rb.position + (Vector3)movement * moveSpeed * Time.fixedDeltaTime);
 
-        Vector3 lookDir = mousePos - (Vector3)rb.position;
+        Vector2 lookDir = mousePos - (Vector2)rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = Quaternion.Euler(0,angle,0);
     }
