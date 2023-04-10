280a829425b3c9f9fda39ddc158ae71267c3e461
zachzhou31 <105928767+zachzhou31@users.noreply.github.com>
2023-04-10 16:05:32 -0400

    Add Behaviour-Tree Structure

--- Assets/Basic Script/PlayerController.cs
+++ Assets/Basic Script/PlayerController.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public Rigidbody rb;
+    public float _rotateSpeed;
+    private Rigidbody rb;
     public Camera cam;
 
-    Vector2 movement;
-    Vector2 mousePos;
-
+    Vector3 movement;
+    Vector3 mousePos;
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
@@ -21,7 +26,7 @@ public class PlayerController : MonoBehaviour
     {
         rb.MovePosition(rb.position + (Vector3)movement * moveSpeed * Time.fixedDeltaTime);
 
-        Vector2 lookDir = mousePos - (Vector2)rb.position;
+        Vector3 lookDir = mousePos - (Vector3)rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = Quaternion.Euler(0,angle,0);
     }
